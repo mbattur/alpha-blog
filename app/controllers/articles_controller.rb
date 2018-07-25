3ebca5f7257af4dfb71d4d0898423318de464a15
Mergen Battur <mbattur@gmail.com>
2018-07-25 13:10:37 -0500

    Set article model2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,44 @@ def new
   @article = Article.new
 end
 
+def create
+  @article = Article.new(article_params)
+  if @article.save
+    flash[:notice] = "Saved"
+    redirect_to article_path(@article)
+  else
+    flash[:notice] = "Try again"
+    render 'new'
+  end
+  
+end
+
+def index
+  @articles = Article.all
+end
+
+def show
+  @article = Article.find(params[:id])
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
+end
+
+
+private
+  def article_params 
+    params.require(:article).permit(:title, :description)
+  end
+
 end
