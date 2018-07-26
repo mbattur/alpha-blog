7ce11d1d5bc356e8100ed7ba3a6b3c7538c643c1
Mergen Battur <mbattur@gmail.com>
2018-07-26 17:39:56 -0500

    Style with bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
 
 def new
   @article = Article.new
@@ -21,22 +22,18 @@ def index
 end
 
 def show
-  @article = Article.find(params[:id])
 end
 
 def destroy
-  @article = Article.find(params[:id])
   @article.destroy
   flash[:notice] = "Deleted"
   redirect_to articles_path
 end
 
 def edit
-  @article = Article.find(params[:id])
 end
 
 def update
-  @article = Article.find(params[:id])
   if @article.update(article_params)
     flash[:notice] = "Updated"
     redirect_to article_path(@article)
@@ -47,6 +44,11 @@ end
 
 
 private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params 
     params.require(:article).permit(:title, :description)
   end
