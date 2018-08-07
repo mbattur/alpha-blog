ecc775d1f9efd993804314b6d642d91391608fe2
Mergen Battur <mbattur@gmail.com>
2018-08-07 10:38:52 -0500

    Add pagination to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @category = Category.new
-
   end
 
   def create
